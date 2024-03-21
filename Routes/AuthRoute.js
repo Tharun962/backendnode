a4338ae5844d532f01e957f064336f23deac7bde
Tharun962 <tharunbheemarthi31@gmail.com>
2024-03-22 01:22:45 +0530

    routes

---
+++ Routes/AuthRoute.js
@@ -0,0 +1,14 @@
+const userModel=require('../Models/UserModel.js');
+const express=require('express');
+
+const router=express.Router();
+
+
+router.post("/register",async(req,res)=>{
+});
+
+router.post("/login",async(req,res)=>{
+});
+
+module.exports=router;
+
--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const dotenv = require("dotenv").config({path: '.env'});
 const cors = require('cors');
-
+const authRoute=require('./Routes/AuthRoute.js');
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    credentials:true,
+}));
 
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use("/",authRoute);
 app.listen("3601", "0.0.0.0", () => {
     console.log("Server running on port 3500");
 });
