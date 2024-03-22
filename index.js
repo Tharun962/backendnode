d31195d7fe6f4ee949327bd0d55a8d46c58ad70b
Tharun962 <tharunbheemarthi31@gmail.com>
2024-03-22 23:01:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const cookieParser=require('cookie-parser');
 
 app.use(cors({
     credentials:true,
-    origin:"https://frontendreact-a17m.onrender.com"
+    origin:"http://frontendreact-a17m.onrender.com"
 }));
 
 app.use(express.urlencoded({extended:true}));
