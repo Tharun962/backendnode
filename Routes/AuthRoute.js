6481da3c4b98387df0f09cff74f515e74b45dc7c
Tharun962 <tharunbheemarthi31@gmail.com>
2024-03-22 12:27:33 +0530

    backend data

--- Routes/AuthRoute.js
+++ Routes/AuthRoute.js
@@ -4,10 +4,68 @@ const express=require('express');
 const router=express.Router();
 
 
+router.get("/logout",(req,res)=>{
+    const cookie=req.cookies.username;
+    if(cookie)
+    {
+        res.cookie("username","",{expires:new Date(Date.now())});
+        res.send({msg:"Logout successfully",logout:true})
+    }
+    else
+    {
+        res.send({msg:"No cookie found",logout:true})
+    }
+});
+
 router.post("/register",async(req,res)=>{
+    const registerBody=req.body;
+    try
+    {
+      let user=userModel.findOne({email:body.email});
+      if(user)
+      {
+        res.send({msg:"User already registered",register:false});
+        return;
+      }
+      else
+      {
+        let create=new userModel(registerBody);
+        await create.save();
+        res.send({msg:"User registered",register:true});
+      }
+    }
+    catch(err)
+    {
+        res.send({msg:err,register:false});
+    }
 });
 
 router.post("/login",async(req,res)=>{
+    const loginBody=req.body;
+    try
+    {
+      let user=userModel.findOne({email:body.email});
+      if(user)
+      {
+        if(user.password===loginBody.password)
+        {
+            res.cookie("username",user.email,{maxAge:900000,httpOnly:true});
+            res.send({msg:"can login",login:true});
+        }
+        else
+        {
+            res.send({msg:"password incorrect",login:false});
+        }
+      }
+      else
+      {
+        res.send({msg:"No user found!",login:false});
+      }
+    }
+    catch(err)
+    {
+        res.send({msg:err,login:false});
+    }
 });
 
 module.exports=router;
--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const dotenv = require("dotenv").config({path: '.env'});
+const dotenv = require("dotenv").config({path: 'config.env'});
 const cors = require('cors');
 const authRoute=require('./Routes/AuthRoute.js');
+const connectDb=require('./Database/Database.js');
 const app = express();
 
 app.use(cors({
@@ -11,6 +12,9 @@ app.use(cors({
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use("/",authRoute);
-app.listen("3601", "0.0.0.0", () => {
-    console.log("Server running on port 3500");
+
+connectDb();
+
+app.listen(process.env.PORT, "0.0.0.0", () => {
+    console.log(`Server running on port: ${process.env.PORT}`);
 });
